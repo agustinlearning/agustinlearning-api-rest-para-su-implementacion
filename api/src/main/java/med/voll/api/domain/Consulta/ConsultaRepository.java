594cb5effa8665650d35cd02d86526edf26b5d68
agustin <agustinalvarez7378@gmail.com>
2025-07-13 11:48:09 -0400

    adding the last validation ValidadorMedicoConOtraconsultaEnElMismoHorario

--- api/src/main/java/med/voll/api/domain/Consulta/ConsultaRepository.java
+++ api/src/main/java/med/voll/api/domain/Consulta/ConsultaRepository.java
@@ -1,6 +1,7 @@
 package med.voll.api.domain.Consulta;
 
 
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,7 @@ import java.time.LocalDateTime;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
     boolean existsByPacienteIdAndFechaBetween(@NotNull Long idPaciente, LocalDateTime primerHorario, LocalDateTime ultimoHorario);
+
+    boolean existsByMedicoIdAndFecha(Long idMedico, @NotNull @Future LocalDateTime fecha);
+
 }
--- api/src/main/java/med/voll/api/domain/Consulta/Validaciones/ValidadorMedicoConOtraconsultaEnElMismoHorario.java
+++ api/src/main/java/med/voll/api/domain/Consulta/Validaciones/ValidadorMedicoConOtraconsultaEnElMismoHorario.java
@@ -1,4 +1,18 @@
 package med.voll.api.domain.Consulta.Validaciones;
 
+import med.voll.api.domain.Consulta.ConsultaRepository;
+import med.voll.api.domain.Consulta.DatosReservarConsulta;
+import med.voll.api.domain.ValidacionException;
+
 public class ValidadorMedicoConOtraconsultaEnElMismoHorario {
+    private ConsultaRepository repository;
+
+    private void validar(DatosReservarConsulta datos){
+
+        var medicoTieneOtraConsultaEnElMismoHorario = repository.existsByMedicoIdAndFecha(datos.idMedico(), datos.fecha());
+
+        if (medicoTieneOtraConsultaEnElMismoHorario) {
+            throw new ValidacionException("El Medico Ya tiene una consulta en esta fecha y hora");
+        }
+    }
 }
